1e8c964a81347e4fceabf339d4056f15777e0ccd
SergeyAbrosov <sgabrosov@yandex.ru>
2022-09-02 17:06:40 +0300

    Added files in .gitignore

--- valt/cmake-build-debug/qdcm/dcmmod/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sergeyabrosov/Desktop/Project/sklif-2022-23/valt/qdcm/dcmmod
-# Build directory: /Users/sergeyabrosov/Desktop/Project/sklif-2022-23/valt/cmake-build-debug/qdcm/dcmmod
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(dcmmod_TestDcmModules "/Users/sergeyabrosov/Desktop/Project/sklif-2022-23/valt/deploy/debug/dcmmod_TestDcmModules")
-set_tests_properties(dcmmod_TestDcmModules PROPERTIES  _BACKTRACE_TRIPLES "/Users/sergeyabrosov/Desktop/Project/sklif-2022-23/valt/cmake/build_test.cmake;52;add_test;/Users/sergeyabrosov/Desktop/Project/sklif-2022-23/valt/cmake/build_test.cmake;0;;/Users/sergeyabrosov/Desktop/Project/sklif-2022-23/valt/qdcm/dcmmod/CMakeLists.txt;8;include;/Users/sergeyabrosov/Desktop/Project/sklif-2022-23/valt/qdcm/dcmmod/CMakeLists.txt;0;")
